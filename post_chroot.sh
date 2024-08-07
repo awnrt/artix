eb4c8bc8be393427672f172d492d20a48493120d
awy <awy@tutamail.com>
2024-08-07 22:02:49 +0300

    small fixes

--- post_chroot.sh
+++ post_chroot.sh
@@ -45,7 +45,7 @@ elif [ "$_kernelflag" -eq 2 ]; then
   make headers_install
   mkdir -p /boot/EFI/BOOT
   cp arch/x86/boot/bzImage /boot/EFI/BOOT/BOOTX64.EFI
-  _diskdrivewop="${diskdrive%p}"
+  _diskdrivewop="${disk_drive%p}"
   efibootmgr -c -d /dev/$_diskdrivewop -p $_numBoot -L "linux" -l '\EFI\BOOT\BOOTX64.EFI'
 else
   printf ${LIGHTRED}"Wrong kernelflag value.${NoColor}\n"
